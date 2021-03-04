e3133941a4afba7ce9f667fc5e9d797bf9404fc6
jle349 <jle349@discovery2.hpc.usc.edu>
2021-03-03 19:19:03 -0800

    test

--- R_code/week4_first_data_analysis.R
+++ R_code/week4_first_data_analysis.R
@@ -1,39 +1,44 @@
-#Installs TCGAbiolinks if not already present
 if (!require(TCGAbiolinks)) BiocManager::install("TCGAbiolinks")
 
-#What line of code needs to be added before using TCGAbiolinks?
+library(TCGAbiolinks)
+library(SummarizedExperiment)
 
 #add barcodes argument to query if you want to run on your local machine for smaller files downloaded
 #barcodes_rnaseq <- c("TCGA-BH-A0DG-01A-21R-A12P-07","TCGA-A2-A0YF-01A-21R-A109-07",
-#          "TCGA-AN-A04A-01A-21R-A034-07","TCGA-AR-A1AP-01A-11R-A12P-07",
-#              "TCGA-A2-A0T3-01A-21R-A115-07", "TCGA-E2-A154-01A-11R-A115-07" )
+ #         "TCGA-AN-A04A-01A-21R-A034-07","TCGA-AR-A1AP-01A-11R-A12P-07",
+  #         "TCGA-A2-A0T3-01A-21R-A115-07", "TCGA-E2-A154-01A-11R-A115-07" )
 #barcodes_clinic <- c("TCGA-BH-A0DG","TCGA-A2-A0YF","TCGA-AN-A04A","TCGA-AR-A1AP", "TCGA-A2-A0T3",
 #                      "TCGA-E2-A154", "TCGA-AO-A12F", "TCGA-A2-A0YM", "TCGA-BH-A0E0", "TCGA-AN-A0FL")
 
 
-#######    Group 1: RNASeq     ############
-# library(SummarizedExperiment)
-# query <- GDCquery(project = "TCGA-BRCA",
-#                   data.category = "Transcriptome Profiling",
-#                   data.type = "Gene Expression Quantification",
-#                   workflow.type = "HTSeq - Counts")
-# GDCdownload(query) #only need this line of code once to download the data
-# sum_exp <- GDCprepare(query)
+query <- GDCquery(project = "TCGA-BRCA",
+                   data.category = "Transcriptome Profiling",
+                   data.type = "Gene Expression Quantification",
+                   workflow.type = "HTSeq - Counts")
+GDCdownload(query) #only need this line of code once to download the data
+sum_exp <- GDCprepare(query)
 # Create a tutorial on SummarizedExperiment
 
+ 
+patient_data <- colData(sum_exp)
+patient_ages <- patient_data$paper_age_at_initial_pathologic_diagnosis
+patient_data$age_category = ifelse(patient_ages < 40, "Young", ifelse(patient_ages >= 60, "Old", "Mid"))
+
+htseq_counts <- assays(sum_exp)$"HTSeq - Counts"
+patient_data$TP53_counts = htseq_counts["ENSG00000141510",]
+patient_data$ERBB2_counts = htseq_counts["ENSG00000141736",]
+patient_data$PIK3CA_counts = htseq_counts["ENSG00000121879",]
+patient_data$ESR1_counts = htseq_counts["ENSG00000091831",]
+
+#genes <- c("ENSG00000141510", "ENSG00000141736", "ENSG00000121879")
+#htseq_counts["ENSG00000141510",]
+
 # Boxplots by age
-# Add a new column to colData called "age_category"
-# If age_at_initial_pathologic_diagnosis is < 40, define patient as "Young" in new column
-# If age_at_initial_pathologic_diagnosis is >= 60, define patient as "Old" in new column
-# Other patients (between 40 and 60), define as "Mid"
-# Choose 3 genes of interest from the paper presentations
-# Create 3 different boxplots with age_category on x-axis, counts on the y-axis by repeating the below code for each gene
-# remember to give your plot a title and informative axis labels
-# png("boxplot_genename.png")
-# boxplot(FILL IN HERE)
-# *Feel free to add lines here that format your boxplot*
-# dev.off()
-# Use rsync to copy your create pngfile to local machine for viewing
+boxplot(TP53_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for TP53 by Age Category")
+boxplot(ERBB2_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for ERBB2 by Age Category")
+boxplot(PIK3CA_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for PIK3CA by Age Category")
+boxplot(ESR1_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for ESR1 by Age Category")
+
 
 #######    Group 2: clinical   ###########
 # library(survival) #What do you need to do before running this line?
